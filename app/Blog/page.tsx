a93fd7dd1ee8c27587914c54f8b941d0bf80371a
unknown <asmar.bpn12@gmail.com>
2026-04-29 12:37:50 +0800

    first commit

---
+++ app/Blog/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function blog() {
+    return (
+        <div>
+            <p>ini adalah halaman BLOG!</p>
+            <input type="text" placeholder="Masukkan teks disini" />
+            <button>Click Me</button>
+        </div>
+    );
+}
---
+++ app/abc/page.tsx
@@ -0,0 +1,9 @@
+export default function abc() {
+    return (
+        <div>
+            <p>ini adalah halaman ABC!</p>
+            <input type="text" placeholder="Masukkan teks disini" />
+            <button>Click Me</button>
+        </div>
+    );
+}
