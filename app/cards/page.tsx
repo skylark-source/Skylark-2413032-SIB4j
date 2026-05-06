87a99a7ec8862cf483903b251e7dd7c862ae10ee
skylark-source <asmar.bpn12@gmail.com>
2026-05-06 22:15:35 +0800

    Add files via upload

---
+++ app/cards/page.tsx
@@ -0,0 +1,17 @@
+export default function cards() {
+    return(
+        <div className="bg-blue-100 min-h-screen p-10 flex items-center justify-center gap-5">
+            <div className="bg-white p-30 rounded-2xl">
+                <div classname="bg-blue-100 p-10 rounded-2xl">
+                    <p className="text-bold text-black mt-2"> Explore </p>
+                    <img src={"/arrow.svg"}/>
+                    <div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
